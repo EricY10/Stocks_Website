072af7b895af8195801ee3c93c617b6d7b632141
OliviaNewell <omnewell@asu.edu>
2024-11-17 17:37:52 -0700

    Update __init__.py

--- Stocky/flask_auth_app/project/__init__.py
+++ Stocky/flask_auth_app/project/__init__.py
@@ -39,7 +39,7 @@ def update_stock_prices():
         try:
             stocks = Stock.query.all()
             for stock in stocks:
-                fluctuation = random.uniform(-0.05, 0.08)
+                fluctuation = random.uniform(-0.05, 0.2)
                 stock.price = round(stock.price * (1 + fluctuation), 2)
             db.session.commit()
             for stock in stocks:
